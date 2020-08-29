a22a70c17c85dfd04005bb418bfc4dd4f840d3d9
kelvin-daniel <kaymutor@gmail.com>
2020-08-29 14:39:03 +0300

    add client id and client secret

--- git-search/src/app/services/profile.service.ts
+++ git-search/src/app/services/profile.service.ts
@@ -10,5 +10,9 @@ export class ProfileService {
     private clientid='84c1e6c7734ac7f567f3';
     private clientsecret = '2c29f0d4677bd9e3490b748416925264e2d6fd2a';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    console.log("service ready!");
+    this.username = 'kelvin-daniel'
+    
+   }
 }
